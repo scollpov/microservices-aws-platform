33f763ed4d3a6ba1bfcdac9f55f10c76c277236c
scoll <sergi@instantcredit.net>
2026-05-11 11:52:49 +0200

    Improve gateway ECS health check stability

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -57,8 +57,8 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
+    from_port       = 8080
+    to_port         = 8080
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }

79e948613f11929c1c5faa4bd5fbe3d96a72fe0b
scoll <sergi@instantcredit.net>
2026-05-14 11:25:53 +0200

    Add unit test execution to service pipelines

--- services/gateway/src/test/java/com/example/gatewayfinalservice/GatewayServiceApplicationTests.java
+++ services/gateway/src/test/java/com/example/gatewayfinalservice/GatewayServiceApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.gatewayfinalservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-class GatewayfinalServiceApplicationTests {
+class GatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
-
 }
--- services/orders/src/test/java/com/example/orderservice/OrderApplicationTests.java
+++ services/orders/src/test/java/com/example/orderservice/OrderApplicationTests.java
@@ -3,13 +3,13 @@ package com.example.orderservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-class OrderserviceApplicationTests {
+class OrderApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
-
-
-
 }

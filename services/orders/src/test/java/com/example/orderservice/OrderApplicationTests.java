005dc86229fa9a11d9edafb65e083874311f4544
scoll <sergi@instantcredit.net>
2026-05-14 11:32:29 +0200

    Adjust unit test execution to service pipelines

--- services/orders/src/test/java/com/example/orderservice/OrderApplicationTests.java
+++ services/orders/src/test/java/com/example/orderservice/OrderApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.orderservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 class OrderApplicationTests {
 
     @Test

cfc8ad5e817fe4ccf436b72ac7057d45f3db6e49
scoll <sergi@instantcredit.net>
2026-05-14 13:44:32 +0200

    Cleaning the code

--- services/payments/src/main/java/com/example/paymentservice/application/CreatePaymentService.java
+++ services/payments/src/main/java/com/example/paymentservice/application/CreatePaymentService.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class CreatePaymentService implements CreatePaymentUseCase {
 
     private static final Logger log =
-        LoggerFactory.getLogger(PaymentKafkaConsumer.class);
+        LoggerFactory.getLogger(CreatePaymentService.class);
 
     private final PaymentRepositoryPort repository;
 
